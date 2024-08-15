c07e72ce5556e7936473d515ba2c3367019baf61
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-08-15 01:09:11 -0300

    promises

--- promises.js
+++ promises.js
@@ -4,15 +4,26 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-function esperaAi(msg, tempo, cb) {
-    setTimeout(() => {
-        console.log(msg);
-        if(cb) cb();
-    }, tempo);
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg)
+        }, tempo);
+    })
 }
 
-esperaAi('Frase 1', rand(1, 3), fucntion() {
-    esperaAi('Frase 2', rand(1, 3), function () {
-        esperaAi('Frase 3', rand(1, 3));
-    });
-});
+esperaAi('Conexão com o BD', rand(1, 3))
+.then(resposta => {
+    console.log(resposta);
+    return esperaAi('Buscando dados da BASE', rand(1, 3));
+})
+.then(resposta => {
+    console.log(resposta);
+})
+.then(() => {
+    console.log('Exibwe dados na tela');
+})
+
+esperaAi('Frase 1', rand(1, 3))
+.then()
+.catch();
