cd9357d20c89cfc5e2368cfdfbbdfab818508ea6
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-08-23 01:21:39 -0300

    util-method-promisses

--- util-method-promisses.js
+++ util-method-promisses.js
@@ -1,33 +1,36 @@
 function rand(min, max) {
-    min *= 1000;
-    max *= 1000;
-    return Math.floor(Math.random() * (max - min) + min);
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function esperaAi(msg, tempo) {
-    return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') reject(false);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Cai no erro");
+        return;
+      }
 
-        setTimeout(() => {
-            resolve(msg.toUpperCase() + ' - Passei na promise');
-        }, tempo);
-    });
+      resolve(msg.toUpperCase() + " - Passei na promise");
+      return;
+    }, tempo);
+  });
 }
 
 //Promise.all Promise.race Promise.resolve Promise.reject
 
 const promises = [
-    'Primeiro valor',
-    esperaAi('Promise 1', 3000),
-    esperaAi('Promise 2', 500),
-    esperaAi('Promise 3', 1000),
-    'Outro valor'
+  esperaAi("Promise 1", rand(1, 5)),
+  esperaAi("Promise 2", rand(1, 5)),
+  esperaAi("Promise 3", rand(1, 5)),
+  esperaAi(1000, rand(1, 5))
 ];
 
-Promise.all(promises)
-.then(function(valor) {
+Promise.race(promises)
+  .then(function (valor) {
     console.log(valor);
-})
-.catch(function(erro) {
+  })
+  .catch(function (erro) {
     console.log(erro);
-})
+  });
